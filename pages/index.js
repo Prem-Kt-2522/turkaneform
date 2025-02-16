7e479908a0769b2253ebbb03e6bc1d1e58a07b0d
Prem-Kt-2522 <145849556+Prem-Kt-2522@users.noreply.github.com>
2025-02-16 16:45:43 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,12 @@
-import CorporateForm from '../components/CorporateForm';
+
+"use client"
+
+import CorporateForm from '../components/CorporateForm'
 
 export default function Home() {
   return (
-    <div className="bg-gray-100 flex justify-center items-center min-h-screen">
-      <main className="flex justify-center items-center w-full">
-        <CorporateForm />
-      </main>
-    </div>
-  );
+    <main className="flex justify-center items-center w-full">
+      <CorporateForm />
+    </main>
+  )
 }
