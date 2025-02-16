c6a8d9081958686b703f92c71d2b389903ea217e
Prem-Kt-2522 <145849556+Prem-Kt-2522@users.noreply.github.com>
2025-02-16 16:51:12 +0530

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -2,8 +2,11 @@ import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="en" className="light">
+      <Head>
+        {/* Force light mode */}
+        <meta name="color-scheme" content="light" />
+      </Head>
       <body className="bg-gray-100">
         <Main />
         <NextScript />
